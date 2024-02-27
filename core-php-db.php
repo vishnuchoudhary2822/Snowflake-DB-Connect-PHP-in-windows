dfbc98ddd7e845e3533f96ffe8d959f890f6c734
Vishnu Choudhary <60024485+vishnuchoudhary2822@users.noreply.github.com>
2024-02-27 17:04:44 +0530

    Update and rename db.php to core-php-db.php

--- core-php-db.php
+++ core-php-db.php
@@ -3,14 +3,20 @@ $host = 'your_account_name.snowflakecomputing.com';
 $account = 'your_account_name';
 $username = 'your_username';
 $password = 'your_password';
-$database = 'your_database';
-$schema = 'your_schema';
+$database = 'your_database_name';
+$schema = 'your_schema_name';
+$warehouse = 'your_warehouse_name';
+$role = 'your_role_name';
 
 try {
     $dsn = "snowflake:host=$host;account=$account;dbname=$database;schema=$schema";
     $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->exec("USE WAREHOUSE $warehouse");
+    $conn->exec("USE ROLE $role");
     echo "Connected to Snowflake successfully";
 } catch (PDOException $e) {
     echo "Failed to connect to Snowflake: " . $e->getMessage();
 }
+
 ?>
